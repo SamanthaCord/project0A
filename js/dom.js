4bddb2afb88ea199c527a875b429fc605b1636d6
Sam Cordingley <sam@cordandberg.com>
2017-11-08 16:50:33 +1100

    game pattern displays, indicates if user guesses correct or wrong

--- js/dom.js
+++ js/dom.js
@@ -1,30 +1,49 @@
 
 //check document is ready//
+let sosButtonClicked = false;
+let buoyButtonClicks = 0;
 
 $(document).ready(function () {
   console.log("document ready");
 
 $('.sosButton').on('click', function (){
+  if (sosButtonClicked === false) {
   console.log("sos button clicked");
+  sosButtonClicked = true;
   pattern(gameBoard);
   console.log(gamePattern);
   for (i = 0; i < gamePattern.length; i++) {
     let highlightColor = document.getElementById(gamePattern[i]);
-    $(highlightColor).css("background-color", "#21E412");
+    $(highlightColor).addClass("circleFlash");
   }
+}
 })
 
 
 $(".buoy").click(function () { $(this)
   //console.log(this);
+  if (buoyButtonClicks < 4) {
   let value = ($(this).attr('id'));
-  console.log(value);
+  //console.log(value);
   if (value === gamePattern[0] || value === gamePattern[1] || value === gamePattern[2] || value === gamePattern[3]) {
     console.log('continue');
     $(this).css("background-color", "#21E412");
+    buoyButtonClicks += 1;
   }
+  else if (buoyButtonClicks < 4) {
+    console.log('wrong!');
+    //wrong text!
+  }
+}
+else if (buoyButtonClicks >= 4) {
+  //trigger next level text!
+  console.log('Next level!');
+  location.reload();
+}
 else {
   console.log('gameover');
+  //time ran out
+  //game over text!
 }
 })
 
